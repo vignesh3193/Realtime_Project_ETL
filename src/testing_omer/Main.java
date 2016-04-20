6e151d4c94d3f6358feeebb8a8626015b91a689c
Omer Solmazer <omersolmazer@Omers-MacBook-Pro.local>
2016-04-20 17:41:19 -0400

    Driver closing bug fixed, inputs separated into folders

--- src/testing_omer/Main.java
+++ src/testing_omer/Main.java
@@ -8,15 +8,16 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.UnreachableBrowserException;
 
 public class Main {
 
 	public static int renthopPageLimit = 10;
 	public static int craigslistPageLimit = 10;
-	
+
 	public static WebDriver renthopBrowser = new FirefoxDriver();
 	public static WebDriver craigslistBrowser = new FirefoxDriver();
-	
+
 	public static void renthopDriver(){
 		for(int i = 0; i < renthopPageLimit; i++){
 			try {
@@ -31,13 +32,13 @@ public class Main {
 			}
 		}
 	}
-	
+
 	public static void getRenthopPages(int page) throws IOException{
 		renthopBrowser.navigate().to("https://www.renthop.com/search/nyc?page="+page);
 
 		String text = renthopBrowser.findElement(By.id("search-results-box")).getText();
 		String[] listings = text.split("Check Availability");
-		FileWriter fileWriter = new FileWriter("renthop-page-"+page+".txt");
+		FileWriter fileWriter = new FileWriter("Renthop/renthop-page-"+page+".txt");
 		for(String s: listings){
 			int index = 0;
 			if(s.charAt(0) == '\n')
@@ -45,16 +46,16 @@ public class Main {
 
 			if(s.contains("No Fee"))
 				index = 1;
-			
+
 			String parsedString[] = s.split("\n");
 			ApartmentObject aptObj = new ApartmentObject(parsedString[index], parsedString[index+2], parsedString[index+1]);
-			fileWriter.write(aptObj.toString());
+			fileWriter.write(aptObj.toString() + "\n");
 //			System.out.println("An apartment: " + aptObj);
 		}
 
 		fileWriter.close();
 	}
-	
+
 	public static void craigslistDriver(){
 		for(int i = 0; i < craigslistPageLimit; i++){
 			try {
@@ -66,48 +67,60 @@ public class Main {
 			} catch (IOException e) {
 				// TODO: handle exception
 				e.printStackTrace();
+			} catch(UnreachableBrowserException e){
+				System.out.println();
+				e.printStackTrace();
 			}
 		}
 	}
-	
+
 	public static void getCraigsPages(int page) throws IOException{
-		craigslistBrowser.navigate().to("https://newyork.craigslist.org/search/mnh/aap?s="+(100*page));
+		String navigationString = "https://newyork.craigslist.org/search/mnh/aap";
+		if(page != 0){
+			navigationString = navigationString.concat("?s="+(100*page));
+		}
+		craigslistBrowser.navigate().to(navigationString);
 		// Javascript part is optional. Just to see if allows to traverse without getting caught 
-//		((JavascriptExecutor)craigslistBrowser).executeScript("scroll(0,400)");
+		((JavascriptExecutor)craigslistBrowser).executeScript("scroll(0,400)");
 		List<WebElement> text = craigslistBrowser.findElements(By.className("row"));
-		FileWriter fileWriter = new FileWriter("craigslist-page-"+page+".txt");
+		FileWriter fileWriter = new FileWriter("Craigslist/craigslist-page-"+page+".txt");
 
 		for(int i = 0; i < text.size(); i++){
 			String temp=text.get(i).getText();
 			fileWriter.write(temp);
 			fileWriter.write("\n");
 		}
-		
+
 		fileWriter.close();
 	}
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		Thread renthopThread = new Thread(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
-				renthopDriver();
+				try{
+					renthopDriver();
+				} finally{
+					renthopBrowser.close();
+				}
 			}
 		});
 		renthopThread.start();
-		
+
 		Thread craigslistThread = new Thread(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
-				craigslistDriver();
+				try{
+					craigslistDriver();
+				} finally{
+					craigslistBrowser.close();
+				}
 			}
 		});
-		craigslistThread.start();
-		
-		renthopBrowser.close();
-		craigslistBrowser.close();
+				craigslistThread.start();
 	}
 }
